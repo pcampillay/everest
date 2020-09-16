174567339fd2dc3e376f379b51d81114c0633415
Pablo Campillay <campillay.pablo@gmail.com>
2020-09-16 02:15:57 -0300

    cambios optimos 3

--- frontend-captcha/src/app/api.service.ts
+++ frontend-captcha/src/app/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +8,20 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public generatorCatpcha(id){
+  public generatorCaptcha(id){
     let url = "http://localhost:3000/api/captcha";
     return this.httpClient.get(url+'/'+id);
+  }
+
+  public validCaptcha(obj){
+    let url = "http://localhost:3000/api/captcha";
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+      })
+    };
+
+    return this.httpClient.post(url, obj, httpOptions);
+
   }
 }
--- frontend-captcha/src/app/captcha/captcha.component.ts
+++ frontend-captcha/src/app/captcha/captcha.component.ts
@@ -10,14 +10,10 @@ import { ApiService } from '../api.service';
 export class CaptchaComponent implements OnInit {
 
   captcha :any;
-
   uploads: any = [];
   sparks: any = [0,0,0,0];
-
-  spark1:any = 0;
-  spark2:any = 0;
-  spark3:any = 0;
-  spark4:any = 0;
+  valids: any = [0,0,0,0];
+  condition: boolean = false;
 
   constructor(
     private apiService: ApiService,
@@ -27,17 +23,12 @@ export class CaptchaComponent implements OnInit {
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    console.log(id);
-    this.apiService.generatorCatpcha(id).subscribe(data => {
+    this.apiService.generatorCaptcha(id).subscribe(data => {
       this.captcha = data;
       let obj = JSON.parse(this.captcha.result.code);
-      console.log(obj);
-
-     this.uploads.push(obj.p1 *90);
-     this.uploads.push(obj.p2 *90);
-     this.uploads.push(obj.p3 *90);
-     this.uploads.push(obj.p4 *90);
-
+      for (let variable in obj) {       
+        this.uploads.push(obj[variable] * 90);
+     }
     });
   }
 
@@ -46,15 +37,39 @@ export class CaptchaComponent implements OnInit {
     return 'rotate('+upload+'deg)';
   }
 
-  setTransformSpark(spark){
-    return 'rotate('+spark+'deg)';
-  }
-
   rotateReturn(spark, index){
     this.sparks[index] = spark - 90;
+    this.validUpload();
   }
 
   rotateAdvance(spark, index){
     this.sparks[index] = spark + 90;
+    this.validUpload();
+  }
+
+  validUpload(){
+    let objValid = {
+      "p1" : this.sparks[0]/90,
+      "p2" : this.sparks[1]/90,
+      "p3" : this.sparks[2]/90,
+      "p4" : this.sparks[3]/90
+    }
+    let stringValid = JSON.stringify(objValid);
+
+    let result = {
+      "id" : this.captcha.result.id,
+      "code" : stringValid
+    }
+
+    this.apiService.validCaptcha(result).subscribe(data => {
+      let dat:any = data;
+      if(dat.result>0){
+        this.condition = true;
+      }
+      else{
+        this.condition = false;
+      }
+    });
+
   }
 }
